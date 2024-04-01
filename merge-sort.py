a999ea83a57092b6273ded014c9c64dea3889813
Tibor Balogh <btibor@gmx.at>
2024-04-01 18:44:43 +0200

    Python

---
+++ merge-sort.py
@@ -0,0 +1,33 @@
+import random
+
+random_arr = [random.randint(0, 100) for _ in range(100)]
+
+def mergeSort(arr):
+    if len(arr) < 2:
+        return arr
+    
+    mid = len(arr) // 2
+    firstHalf = arr[:mid]
+    secondHalf = arr[mid:]
+
+    sortedFirstHalf = mergeSort(firstHalf)
+    sortedSecondHalf = mergeSort(secondHalf)
+
+    sortedArr = []
+
+    i = 0
+    j = 0
+    while i < len(sortedFirstHalf) and j < len(sortedSecondHalf):
+        if sortedFirstHalf[i] <= sortedSecondHalf[j]:
+            sortedArr.append(sortedFirstHalf[i])
+            i += 1
+        else:
+            sortedArr.append(sortedSecondHalf[j])
+            j += 1
+
+    sortedArr.extend(sortedFirstHalf[i:])
+    sortedArr.extend(sortedSecondHalf[j:])
+    
+    return sortedArr
+
+print(mergeSort(random_arr))
